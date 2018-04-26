d39e297112cfb8a95dc71f91f86de8a3d294b379
elton jefferson <eltonjncorreia@gmail.com>
2018-04-26 16:08:20 -0300

    rota de cadastro

--- conceptus/core/urls.py
+++ conceptus/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, create
 
 app_name = 'core'
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('cadastro/', create, name='create'),
 ]
