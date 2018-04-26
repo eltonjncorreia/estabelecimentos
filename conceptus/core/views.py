0373b3ef09fcb3264133a3a8a93f31b5a0c3e2d8
elton jefferson <eltonjncorreia@gmail.com>
2018-04-26 16:16:28 -0300

    views de cadastro de loja

--- conceptus/core/views.py
+++ conceptus/core/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, resolve_url as r
 
+from conceptus.core.forms import StoreModelForm
 from conceptus.core.models import Store
 
 
 def dashboard(request):
     stores = Store.objects.all()
     return render(request, 'core/dashboard.html', {'stores': stores})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = StoreModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(r('core:dashboard'))
+    else:
+        form = StoreModelForm()
+
+    return render(request, 'core/cadastro.html', {'form': form})
+
+
+
+
+
+
+
