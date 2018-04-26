f4e3be35bfaf1928db82b12ef906fbb5a76670cb
elton jefferson <eltonjncorreia@gmail.com>
2018-04-26 15:37:24 -0300

    model store

--- conceptus/core/models.py
+++ conceptus/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class Store(models.Model):
+    ATUACAO = (('A', 'Alimentos'),
+               ('Auto', 'Automóveis'),
+               ('Esp', 'Esport'))
+
+    ramo_atuacao = models.CharField(max_length=4, choices=ATUACAO)
+    nome = models.CharField('Nome', max_length=255)
+    slug = models.SlugField('Slug')
+    descricao = models.CharField('Descrição', max_length=255)
+    contato = models.IntegerField('Contato', validators=[RegexValidator(r'^\d+$')])
+
+    class Meta:
+        ordering = ['nome']
+        verbose_name = 'Store'
+        verbose_name_plural = 'Stores'
+
+    def __str__(self):
+        return self.nome
+
