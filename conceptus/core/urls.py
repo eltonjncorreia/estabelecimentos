15653db6bab904faa024e5ab799618e788e7aa14
elton jefferson <eltonjncorreia@gmail.com>
2018-04-26 22:53:05 -0300

    rota para editar

--- conceptus/core/urls.py
+++ conceptus/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import dashboard, create, detail
+from .views import dashboard, create, editar
 
 app_name = 'core'
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('cadastro/', create, name='create'),
-    path('detail/<slug:slug>/', detail, name='detail'),
+    path('editar/<slug:slug>/', editar, name='editar'),
 ]
