1b3ec30eba8bcb7abc5283e250c9f97ab15e47eb
elton jefferson <eltonjncorreia@gmail.com>
2018-04-27 02:00:12 -0300

    teste context exist na view

--- conceptus/core/tests/test_dashboard.py
+++ conceptus/core/tests/test_dashboard.py
@@ -1,13 +1,19 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
+
+from conceptus.core.models import Store
 
 
 class DashboadTest(TestCase):
     def setUp(self):
-        self.resp = self.client.get('/')
+        self.resp = self.client.get(r('core:dashboard'))
 
     def test_get(self):
         self.assertEqual(200, self.resp.status_code)
 
-
     def test_template(self):
         self.assertTemplateUsed(self.resp, 'core/dashboard.html')
+
+    def test_context_stores(self):
+        store = self.resp.context['stores']
+        self.assertTrue(self.resp, store)
